0ffcc7f85f77f0293abefd0d6e30630012953bc5
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2023-11-07 11:39:02 +0000

    product page
    product page w edit and delete functionality

--- admin/pages/api/products.js
+++ admin/pages/api/products.js
@@ -5,11 +5,30 @@ import {Product} from "../../models/Product";
 export default async function handle(req, res){
   const {method} = req;
   await mongooseConnect()
-  if(method=== 'POST'){
+  if(method === 'GET'){
+    if(req.query?.id){
+      res.json(await Product.findOne({_id:req.query.id}))
+    }else{
+      res.json(await Product.find())
+    }
+  }
+
+  if(method === 'POST'){
     const {title,description, price} = req.body;
     const productDoc = await Product.create({
       title, description, price
     })
     res.json(productDoc)
+  }
+  if(method === 'PUT'){
+    const {_id,title,description, price} = req.body;
+    await Product.findOneAndUpdate({_id}, {title, description, price})
+    res.json('success')
+  }
+  if(method=== 'DELETE'){
+    if(req.query?.id){
+      await Product.deleteOne({_id: req.query.id})
+      res.json(true)
+    }
   }
 }
