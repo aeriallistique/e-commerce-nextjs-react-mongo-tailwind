12de3189278008e1f651943e19a3c54f9038cbc0
aeriallistique <67158519+aeriallistique@users.noreply.github.com>
2023-11-23 13:36:10 +0000

    connected front to mongodb
    passed models from admin to front to access db

--- front/components/Button.js
+++ front/components/Button.js
@@ -1,14 +1,14 @@
 import styled,{css} from 'styled-components';
 
-const StyledButton = styled.button`
- 
+export const ButtonStyle = css`
   border:0;
- 
+  
   padding: 5px 15px;
   border-radius: 5px;
   cursor: pointer;
   display: inline-flex;
-  align-items:center;
+  align-items:center; 
+  text-decoration: none;
   svg{
     height: 16px;
     margin-right: 5px;
@@ -31,6 +31,9 @@ const StyledButton = styled.button`
       height: 20px;
     }
     `}
+`;
+const StyledButton = styled.button`
+ ${ButtonStyle} 
 `;
 
 export default function Button({children,...rest}){
